308eb404f3b688d613c8573cd3cda9ed43a0a138
CeutaRossmary <77645395+CeutaRossmary@users.noreply.github.com>
2024-08-26 13:15:01 -0400

    Add files via upload
    Portafolio para Bootcamp DESARROLLO DE APLICACIONES FULL STACK JAVASCRIPT TRAINEE

---
+++ PORTAFOLIO/JS/funciones.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+$(document).ready(function () {
+  $(".bi-person-fill").mouseover(function () {
+    $(".bi-person-fill").css("background-color", "#4ba");
+  });
+  $(".bi-person-fill").mouseout(function () {
+    $(".bi-person-fill").css("background-color", "black");
+  });
+
+  $(".bi-wallet-fill").mouseover(function () {
+    $(".bi-wallet-fill").css("background-color", "#4ba");
+  });
+  $(".bi-wallet-fill").mouseout(function () {
+    $(".bi-wallet-fill").css("background-color", "black");
+  });
+
+   $(".bi-file-text-fill").mouseover(function () {
+     $(".bi-file-text-fill").css("background-color", "#4ba");
+   });
+   $(".bi-file-text-fill").mouseout(function () {
+     $(".bi-file-text-fill").css("background-color", "black");
+   });
+
+    $(".bi-envelope").mouseover(function () {
+      $(".bi-envelope").css("background-color", "#4ba");
+    });
+    $(".bi-envelope").mouseout(function () {
+      $(".bi-envelope").css("background-color", "black");
+    });
+
+     $("#pdf").mouseover(function () {
+       $("#pdf").css("background-color", "#4ba");
+     });
+     $("#pdf").mouseout(function () {
+       $("#pdf").css("background-color", "black");
+     });
+
+
+      $(".bi-linkedin").mouseover(function () {
+        $(".bi-linkedin").css("background-color", "#4ba");
+      });
+      $(".bi-linkedin").mouseout(function () {
+        $(".bi-linkedin").css("background-color", "black");
+      });
+
+       $(".bi-github").mouseover(function () {
+         $(".bi-github").css("background-color", "#4ba");
+       });
+       $(".bi-github").mouseout(function () {
+         $(".bi-github").css("background-color", "black");
+       });
+});
+
+
+""
+
+
+
+
+
